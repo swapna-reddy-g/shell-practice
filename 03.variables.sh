949dfcf5b61a76e1b54b2285bede9c33b48b5eb8
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 10:25:05 +0530

    Variables.sh

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 TIMESTAMP=$(date)
+START_TIME=$(date +%s)
+
+sleep 10s
 
 echo "TIme is $TIMESTAMP"
 
@@ -11,4 +14,8 @@ echo "Your User Name is $USER_NAME"
 
 echo "Please Enter your Password:: "
 read -s Password
- 
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Total Time : $TOTAL_TIME"
