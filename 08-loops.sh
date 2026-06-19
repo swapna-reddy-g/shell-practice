437fc7972163ce175b38984ff51c2de8eedad36b
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 15:02:00 +0530

    08-loops.sh

--- 08-loops.sh
+++ 08-loops.sh
@@ -2,6 +2,10 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date "+%d-%m-%y %H:%M:$S")
+R="\e[31m"
+G="\e32m"
+Y="\e33m"
+N="\e0m"
 
 # Check if root access or not
 if [ $USERID -ne 0 ]; then
@@ -11,9 +15,9 @@ fi
 
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "$TIMESTAMP [ERROR] Installing $1 has Failed" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP [ERROR] Installing $1 has $R Failed $N" | tee -a $LOGS_FILE
     else
-        echo "$TIMESTAMP [INFO] Installing $! is Success" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP [INFO] Installing $! is $G Success $N" | tee -a $LOGS_FILE
     fi
 }
 
@@ -25,6 +29,6 @@ do
         dnf install $package -y | tee -a $LOGS_FILE
         VALIDATE "INstalling $package" $?
     else
-        echo "$package already installed"
+        echo "$package $Y already installed $N"
     fi
 done
