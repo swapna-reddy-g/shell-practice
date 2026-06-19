ae2f1de45b148cb4a7e540550b87093cb94965ce
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 11:13:32 +0530

    05-data-types.sh

--- 05-datatypes.sh
+++ 05-datatypes.sh
@@ -6,3 +6,10 @@ NUM2=30
 SUM=$(($NUM1+$NUM2))
 
 echo "sum is $SUM"
+
+# Arrays
+MOVIES=("RRR", "Bahubali", "varanasi")
+
+echo "Movies ${MOVIES[@]}"
+echo "Movie 1 ${MOVIES[0]}"
+echo "Movie 2 ${MOVIES[1]}"
