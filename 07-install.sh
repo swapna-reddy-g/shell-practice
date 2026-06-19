adfd4e13cb0c578496b92d9fdb7809565eb25e08
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 11:35:08 +0530

    07-install.sh

--- 07-install.sh
+++ 07-install.sh
@@ -6,6 +6,15 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
+    exit 1
 fi
 
-echo " I am Continuing"
+echo "Installing MySQL"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installation Failed, Please try again"
+else
+    echo "Installation Success"
+fi
+
