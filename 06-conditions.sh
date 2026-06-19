a251ac754a86484b68d0e846126a9ad9c73a1c77
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 11:22:25 +0530

    06-conditions.sh

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -2,8 +2,8 @@
 
 NUM=$1
 
-if [$NUM -ge 20]; then
-    echo "$NUM is Greater than 20"
+if [$NUM -ge 20];
+    echo "$NUM is > 20"
 else
-    echo "$NUM is less than 20"
+    echo "$NUM is < 20"
 fi
