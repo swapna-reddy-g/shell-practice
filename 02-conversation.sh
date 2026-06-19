f8a98109af923881c44ae4772067e5ca492844db
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 09:56:54 +0530

    Conversation.sh

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "T: We are going to attack you!"
-echo "I: Yes Please, You're welcome"
-echo "T: Hey, I'm Serious!!"
-echo "I: We are also serious Mr.T"
+# Variables
+person1=Russia
+person2=Ukraine
+
+# Conversation
+echo "$person1: Hey $person2 We are going to attack you!"
+echo "person2: Yes Please, You're welcome"
+echo "$person1: Hey, I'm Serious!!"
+echo "person2: We are also serious Mr.$person1"
