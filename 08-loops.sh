0dcb1fc52012817a1c55b38b2bd1c6d80c2d9383
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 14:44:52 +0530

    08-loops.sh

---
+++ 08-loops.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+# Check if root access or not
+if [ USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 has Failed"
+    else
+        echo "Installing $! is Success"
+    fi
+}
+
+for package in $@
+do
+    echo "Installing $PA
+done
