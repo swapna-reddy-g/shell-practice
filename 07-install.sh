736fab98bc4074f50cb7d762361e18e35428e138
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 11:51:03 +0530

    07-install.sh

--- 07-install.sh
+++ 07-install.sh
@@ -8,18 +8,32 @@ if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi
+
+# $1 --> Name of installation
+# $2 --> exit code
+
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 has Failed, Please try again"
+    else
+        echo "Installing $1 is Success"
+    fi
+}
 dnf list installed mysql
 
 if [ $? -ne 0 ]; then
     echo "Installing MySQL"
     dnf install mysql -y
+    VALIDATE MYSQL $?
 else
     echo "MYSQL is already installed"
 fi
 
+dnf list installed nginx
 if [ $? -ne 0 ]; then
-    echo "Installation Failed, Please try again"
+    echo "Installing NGINX"
+    dnf install nginx -y
+    VALIDATE NGINX $?
 else
-    echo "Installation Success"
+    echo "NGINX is already installed"
 fi
-
