06d51b568621a52b016a2e1813dd11dee81fc7a7
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 11:42:25 +0530

    07-install.sh

--- 07-install.sh
+++ 07-install.sh
@@ -8,9 +8,14 @@ if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi
+dnf list installed mysql
 
-echo "Installing MySQL"
-dnf install mys98uhql -y
+if [ $? -ne 0 ]; then
+    echo "Installing MySQL"
+    dnf install mysql -y
+else
+    echo "MYSQL is already installed"
+fi
 
 if [ $? -ne 0 ]; then
     echo "Installation Failed, Please try again"
